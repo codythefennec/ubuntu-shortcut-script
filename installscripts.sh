d896c222a840716851f73d99233dad5295efd80b
Cody Yuki <101runt52@gmail.com>
2024-12-03 22:15:26 +0000

    Adding the installscripts

---
+++ installscripts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -d "~/Scripts/" ]; then
+	echo "Scripts folder exists.."
+else
+	echo "Scripts folder does not exist.."
+
+	echo "Making scripts folder.."
+
+	mkdir ~/Scripts
+fi
+
+echo "Moving shortcut script.."
+
+mv makeshortcut.sh ~/Scripts
+
+echo "Connecting script to .bashrc.."
+
+echo "source ~/Scripts/makeshortcut.sh" >> ~/.bashrc
+
+echo "Installation complete!"
